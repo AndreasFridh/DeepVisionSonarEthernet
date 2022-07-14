4ba277f6e2ac5f79de151f01e152df11a078d10e
AndreasFridh <46330513+AndreasFridh@users.noreply.github.com>
2022-07-14 22:52:38 +0200

    Update run.sh
    Change order of SimpleHTTPserver and sonar connector. If conntector fails, web server is still running

--- EthernetSonar/run.sh
+++ EthernetSonar/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-	./EthernetSonar 100 output 1
 	python -m SimpleHTTPServer 80 & # If server is dead, try to start again. If port is taken this will default to exit.
+	./EthernetSonar 100 output 1
 	sleep 10s
 done
